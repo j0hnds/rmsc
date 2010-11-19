fd4e9d89c3e2cffd1ee7137588ac4ece3030bc31
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-11-19 14:28:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmsc::Application.config.secret_token = '1d50e6a5497e6f1724a38b4ca082b9ef8a7ad5d5237b093bc34bcf46bb3c3e05e0cd8fdb9d5812283026012238e5e8a31624059908f020e679d9bff98bcb6221'
