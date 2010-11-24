00dcafa59e7093103bd788060bb94bfa81bd472e
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-11-24 09:39:15 -0700

    Updated to use new validations and display validation errors.

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,27 +1,19 @@
 class Coordinator < ActiveRecord::Base
   include RegularExpressions
 
-  validates_presence_of(:first_name, :last_name, :email, :phone)
-  validates_length_of(:first_name, 
-                      :maximum => 20, 
-                      :allow_nil => true, 
-                      :allow_blank => true)
-  validates_length_of(:last_name, 
-                      :maximum => 20, 
-                      :allow_nil => true, 
-                      :allow_blank => true)
-  validates_length_of(:email,
-                      :maximum => 255,
-                      :allow_nil => true,
-                      :allow_blank => true)
-  validates_format_of(:phone,
-                      :with => RePhone,
-                      :allow_nil => true,
-                      :allow_blank => true)
-  validates_format_of(:email,
-                      :with => ReEmail,
-                      :allow_nil => true,
-                      :allow_blank => true)
+  validates :first_name, :presence => true,
+                         :length => { :minimum => 1, :maximum => 20 }
+
+  validates :last_name, :presence => true,
+                        :length => { :minimum => 1, :maximum => 20 }
+
+  validates :email, :presence => true,
+                    :length => { :minimum => 1, :maximum => 255 },
+                    :format => { :with => ReEmail }
+
+  validates :phone, :presence => true,
+                    :length => { :minimum => 1, :maximum => 14 },
+                    :format => { :with => RePhone }
 
   scope :ordered_by_name, order("coordinators.last_name ASC, coordinators.first_name ASC")
 end
