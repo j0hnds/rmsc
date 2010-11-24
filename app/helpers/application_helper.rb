ac775bc3651d0f00cdfe153235c8e62da114ce5b
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-11-24 09:13:19 -0700

    Added required helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
   def form_label_field(f, field_name)
-    "#{f.label field_name}: #{f.text_field field_name}".html_safe
+    "#{f.label field_name}#{mark_required f.object, field_name}: #{f.text_field field_name}".html_safe
+  end
+
+  def mark_required(target, field_name)
+    "*" if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
   end
 end
