ff7f8de5f0e2df32638c81b6cfeef7418d76cee3
j0hnds@gmail.com <j0hnds@gmail.com>
2010-11-26 12:30:05 -0700

    Added will_paginate to deal with lists

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -15,7 +15,5 @@ class Coordinator < ActiveRecord::Base
                     :length => { :minimum => 1, :maximum => 14 },
                     :format => { :with => RePhone }
 
-  self.per_page = 5
-
   scope :ordered_by_name, order("coordinators.last_name ASC, coordinators.first_name ASC")
 end
