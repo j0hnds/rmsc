6333fe17a9be87491e6232a61fe40a9a1f9bede1
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-12-15 15:09:59 -0700

    Added stylesheets for form styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
 
   def form_label_field(f, field_name)
-    "#{f.label field_name}#{mark_required f.object, field_name}: #{f.text_field field_name}".html_safe
+    "#{f.label field_name, mark_required(f.object, field_name)}#{f.text_field field_name, :class => 'text'}".html_safe
   end
 
   def mark_required(target, field_name)
-    "*" if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
+    options = {}
+    options[:class] = 'required' if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
+    options
   end
 end
