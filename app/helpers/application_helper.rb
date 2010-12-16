9b4552c3b6e289bbb2569bb24753fb866c0f7062
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-12-16 08:54:54 -0700

    More styling of forms; looks much better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,11 @@ module ApplicationHelper
   end
 
   def mark_required(target, field_name)
-    options = {}
-    options[:class] = 'required' if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
-    options
+#    options = {}
+#    options[:class] = 'required' if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
+#    options
+    label = "#{field_name.to_s.humanize}:"
+    label = "#{field_name.to_s.humanize}: <em><img src=\"images/required-star.png\" alt=\"required\" /></em>" if target.class.validators_on(field_name).map(&:class).include? ActiveModel::Validations::PresenceValidator
+    label.html_safe
   end
 end
