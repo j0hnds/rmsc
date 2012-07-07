87f2ea87863340f652b7fb5aa650e7776641efae
Dave Sieh <dsieh@luna>
2012-07-07 17:20:10 -0600

    Fixed the legacy convert to read directly from the old PG db and construct the objects in the new PG db.

--- lib/scripts/legacy_convert.rb
+++ lib/scripts/legacy_convert.rb
@@ -37,7 +37,7 @@ SELECT
   location_state,
   location_postal_code
 FROM
-  `show`
+  show
 ORDER BY
   start_date DESC
 EOL
@@ -168,19 +168,26 @@ $exhibitor_map = {}
 $store_map = {}
 $buyer_map = {}
 
+  # 0 al.attendee_type,
+  # 1 al.attendee_id,
+  # 2 al.show_id,
+  # 3 al.line,
+  # 4 al.priority,
+  # 5 ex.first_name,
+  # 6 ex.last_name
 def create_attendee_line(row)
   # The issue here is that lines used to be assigned to an exhibitor/associate
   # for a particular show. Now, the lines are associated at the room level.
   # So, behave accordingly.
   room = nil
-  # puts "### Working on attendee line with type: #{row[0]}:#{row[0].class.name}"
-  case row[0]
+  # puts "### Working on attendee line with type: #{row['attendee_type']}:#{row['attendee_type'].class.name}"
+  case row['attendee_type']
   when '1' # This is an exhibitor
     registration = Registration.where("show_id = ? AND exhibitor_id = ?",
-                                      $show_map[row[2]],
-                                      $exhibitor_map[row[1]]).first
+                                      $show_map[row['show_id']],
+                                      $exhibitor_map[row['attendee_id']]).first
     if registration.nil?
-      puts "Unable to find registration for exhibitor: show(#{row[2]}, #{$show_map[row[2]]}) exhibitor(#{row[1]}, #{$exhibitor_map[row[1]]})"
+      puts "Unable to find registration for exhibitor: show(#{row['show_id']}, #{$show_map[row['show_id']]}) exhibitor(#{row['attendee_id']}, #{$exhibitor_map[row['attendee_id']]})"
       return
     end
     
@@ -189,44 +196,51 @@ def create_attendee_line(row)
 
   when '2' # This is an associate
     room = Room.joins(:registration, :associates).where("show_id = ? AND associates.first_name = ? AND associates.last_name = ?",
-                                                        $show_map[row[2]],
-                                                        row[5],
-                                                        row[6]).first
+                                                        $show_map[row['show_id']],
+                                                        row['first_name'],
+                                                        row['last_name']).first
                                  
   else # This is bad
-    raise "Invalid attendee type: #{row[0]}"
+    raise "Invalid attendee type: #{row['attendee_type']}"
   end
 
   if room.nil?
-    puts "No room was found for lines! (show(#{row[2]}, #{$show_map[row[2]]}) #{row[5]} #{row[6]}"
+    puts "No room was found for lines! (show(#{row['show_id']}, #{$show_map[row['show_id']]}) #{row['first_name']} #{row['last_name']}"
     return
   end
 
-  line = room.lines.where("`order` = ? AND line = ?",
-                          row[4],
-                          row[3]).first
+  line = room.lines.where("lines.order = ? AND line = ?",
+                          row['priority'],
+                          row['line']).first
   if line.nil?
     line = Line.new
-    line.order = row[4]
-    line.line = row[3]
+    line.order = row['priority']
+    line.line = row['line']
 
     room.lines << line
   end
 
 end
 
+  # 0 a.associate_id,
+  # 1 a.exhibitor_id,
+  # 2 a.first_name,
+  # 3 a.last_name,
+  # 4 aa.show_id,
+  # 5 aa.room_assignment
+
 def create_associate(row)
   # Find the associated exibitor registration
   registration = Registration.where("show_id = ? AND exhibitor_id = ?", 
-                                    $show_map[row[4]],
-                                    $exhibitor_map[row[1]]).first
+                                    $show_map[row['show_id']],
+                                    $exhibitor_map[row['exhibitor_id']]).first
   raise "Unable to find registration for associate" if registration.nil?
 
   # Find the room associated with the registration
-  room = registration.rooms.where("room = ?", row[5]).first
+  room = registration.rooms.where("room = ?", row['room_assignment']).first
   if room.nil?
     room = Room.new
-    room.room = row[5]
+    room.room = row['room_assignment']
 
     # Add the room
     registration.rooms << room
@@ -234,32 +248,39 @@ def create_associate(row)
 
   # Now that we have a room, we can create the associate
   associate = Associate.new
-  associate.first_name = row[2]
-  associate.last_name = row[3]
+  associate.first_name = row['first_name']
+  associate.last_name = row['last_name']
 
   # Add the associate to the room
   room.associates << associate
 end
 
+  # 0 show_id,
+  # 1 buyer_id
+
 def create_attendance(row)
   attendance = Attendance.new
-  attendance.show_id = $show_map[row[0]]
-  attendance.buyer_id = $buyer_map[row[1]]
+  attendance.show_id = $show_map[row['show_id']]
+  attendance.buyer_id = $buyer_map[row['buyer_id']]
 
   attendance.save!
 
   attendance
 end
 
+  # 0 show_id,
+  # 1 exhibitor_id,
+  # 2 room_assignment
+
 def create_registration_room(row)
   registration = Registration.new
-  registration.show_id = $show_map[row[0]]
-  registration.exhibitor_id = $exhibitor_map[row[1]]
+  registration.show_id = $show_map[row['show_id']]
+  registration.exhibitor_id = $exhibitor_map[row['exhibitor_id']]
 
   registration.save!
 
   room = Room.new
-  room.room = row[2]
+  room.room = row['room_assignment']
 
   # Add the room to the registration
   registration.rooms << room
@@ -267,57 +288,107 @@ def create_registration_room(row)
   registration
 end
 
+  # 0 buyer_id,
+  # 1 store_id,
+  # 2 first_name,
+  # 3 last_name,
+  # 4 email,
+  # 5 cell
+
 def create_buyer(row)
   buyer = Buyer.new
-  buyer.store_id = $store_map[row[1]]
-  buyer.first_name = row[2]
-  buyer.last_name = row[3]
-  buyer.phone = row[5]
-  buyer.email = clean_email(row[4])
+  buyer.store_id = $store_map[row['store_id']]
+  buyer.first_name = row['first_name']
+  buyer.last_name = row['last_name']
+  buyer.phone = row['cell']
+  buyer.email = clean_email(row['email'])
+
+  # puts "Buyer #{row['buyer_id']} (Store ID: #{row['store_id']}): #{buyer.inspect}"
 
   buyer.save!
 
   buyer
 end
 
+  # 0 s.store_id,
+  # 1 c.name,
+  # 2 s.address_1,
+  # 3 s.address_2,
+  # 4 s.city,
+  # 5 s.state,
+  # 6 s.postal_code,
+  # 7 s.phone,
+  # 8 s.fax
+
 def create_store(row)
   store = Store.new
-  store.name = row[1]
-  store.address = row[2]
-  store.city = row[4]
-  store.state = row[5]
-  store.postal_code = row[6]
-  store.phone = row[7]
-  store.fax = row[8]
+  store.name = row['name']
+  store.address = row['address_1']
+  store.city = row['city']
+  store.state = row['state']
+  store.postal_code = row['postal_code']
+  store.phone = row['phone']
+  store.fax = row['fax']
+
+  # puts "Store: #{row['store_id']}: #{store.inspect}"
 
   store.save!
 
   store
 end
 
+  # 0 exhibitor_id,
+  # 1 first_name,
+  # 2 last_name,
+  # 3 address_1,
+  # 4 address_2,
+  # 5 city,
+  # 6 state,
+  # 7 postal_code,
+  # 8 phone,
+  # 9 fax,
+  #10 cell,
+  #11 email
+
 def create_exhibitor(row)
   exhibitor = Exhibitor.new
-  exhibitor.first_name = row[1]
-  exhibitor.last_name = row[2]
-  exhibitor.address = row[3]
-  exhibitor.city = row[5]
-  exhibitor.state = row[6]
-  exhibitor.postal_code = row[7]
-  exhibitor.phone = row[8]
-  exhibitor.fax = row[9]
-  exhibitor.cell = row[10]
-  exhibitor.email = clean_email(row[11])
+  exhibitor.first_name = row['first_name']
+  exhibitor.last_name = row['last_name']
+  exhibitor.address = row['address_1']
+  exhibitor.city = row['city']
+  exhibitor.state = row['state']
+  exhibitor.postal_code = row['postal_code']
+  exhibitor.phone = row['phone']
+  exhibitor.fax = row['fax']
+  exhibitor.cell = row['cell']
+  exhibitor.email = clean_email(row['email'])
 
   exhibitor.save!
 
   exhibitor
 end
 
+  # 0 show_id,
+  # 1 description,
+  # 2 start_date,
+  # 3 end_date,
+  # 4 location,
+  # 5 coordinator,
+  # 6 location_phone,
+  # 7 location_fax,
+  # 8 location_reservation,
+  # 9 coordinator_phone,
+  #10 coordinator_email,
+  #11 location_address,
+  #12 location_city,
+  #13 location_state,
+  #14 location_postal_code
+
 def create_show(row, venue, coordinator)
   show = Show.new(:venue => venue, :coordinator => coordinator)
-  show.name = row[1]
-  show.start_date = row[2]
-  show.end_date = row[3]
+  show.name = row['description']
+  show.start_date = row['start_date']
+  show.end_date = row['end_date']
   show.next_start_date, show.next_end_date = show.show_dates_after_date(show.start_date)
 
   show.save!
@@ -325,29 +396,61 @@ def create_show(row, venue, coordinator)
   show
 end
 
+  # 0 show_id,
+  # 1 description,
+  # 2 start_date,
+  # 3 end_date,
+  # 4 location,
+  # 5 coordinator,
+  # 6 location_phone,
+  # 7 location_fax,
+  # 8 location_reservation,
+  # 9 coordinator_phone,
+  #10 coordinator_email,
+  #11 location_address,
+  #12 location_city,
+  #13 location_state,
+  #14 location_postal_code
+
 def create_venue(row)
   venue = Venue.new
-  venue.name = row[4]
-  venue.address_1 = row[11]
-  venue.city = row[12]
-  venue.state = row[13]
-  venue.postal_code = row[14]
-  venue.phone = row[6]
-  venue.fax = row[7]
-  venue.reservation = row[8]
+  venue.name = row['location']
+  venue.address_1 = row['location_address']
+  venue.city = row['location_city']
+  venue.state = row['location_state']
+  venue.postal_code = row['location_postal_code']
+  venue.phone = row['location_phone']
+  venue.fax = row['location_fax']
+  venue.reservation = row['location_reservation']
 
   venue.save!
 
   venue
 end
 
+  # 0 show_id,
+  # 1 description,
+  # 2 start_date,
+  # 3 end_date,
+  # 4 location,
+  # 5 coordinator,
+  # 6 location_phone,
+  # 7 location_fax,
+  # 8 location_reservation,
+  # 9 coordinator_phone,
+  #10 coordinator_email,
+  #11 location_address,
+  #12 location_city,
+  #13 location_state,
+  #14 location_postal_code
+
 def create_coordinator(row)
-  first_name, last_name = row[5].split
+  first_name, last_name = row['coordinator'].split
   coordinator = Coordinator.new
   coordinator.first_name = first_name
   coordinator.last_name = last_name
-  coordinator.email = clean_email(row[10])
-  coordinator.phone = row[9]
+  coordinator.email = clean_email(row['coordinator_email'])
+  coordinator.phone = row['coordinator_phone']
 
   coordinator.save!
 
@@ -371,7 +474,7 @@ def process_shows(my)
 
     show = create_show(row, venue, coordinator)
 
-    $show_map[row[0]] = show.id
+    $show_map[row['show_id']] = show.id
 
     index += 1
   end
@@ -383,7 +486,7 @@ def process_exhibitors(my)
   res.each do | row |
     exhibitor = create_exhibitor(row)
     
-    $exhibitor_map[row[0]] = exhibitor.id
+    $exhibitor_map[row['exhibitor_id']] = exhibitor.id
   end
 end
 
@@ -393,7 +496,7 @@ def process_stores(my)
   res.each do | row |
     store = create_store(row)
 
-    $store_map[row[0]] = store.id
+    $store_map[row['store_id']] = store.id
   end
 end
 
@@ -403,7 +506,7 @@ def process_buyers(my)
   res.each do | row |
     buyer = create_buyer(row)
 
-    $buyer_map[row[0]] = buyer.id
+    $buyer_map[row['buyer_id']] = buyer.id
   end
 end
 
@@ -439,7 +542,8 @@ def process_attendee_lines(my)
   end
 end
 
-my = Mysql.new('localhost', 'root', 'p0rKbellie$', 'rmsc_legacy')
+my = PG.connect :dbname => 'RMSC', :user => 'dms', :password => 'j0rdan32'
+# my = Mysql.new('localhost', 'root', 'p0rKbellie$', 'rmsc_legacy')
 
 # Clear out the database
 Associate.destroy_all
